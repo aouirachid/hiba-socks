cb9bd50b1c1e181a1ec98bc60b43c4982f325ce0
aouirachid <aouirachid22@gmail.com>
2025-12-01 10:37:33 +0100

    Update UI element by removing stock and Taux TVA

--- COMPOSANT/Form42.Designer.cs
+++ COMPOSANT/Form42.Designer.cs
@@ -29,33 +29,19 @@ namespace FD_STOCK
         /// </summary>
         private void InitializeComponent()
         {
-            this.button36 = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.pah = new System.Windows.Forms.TextBox();
-            this.st = new System.Windows.Forms.TextBox();
             this.nda = new System.Windows.Forms.TextBox();
             this.nr = new System.Windows.Forms.TextBox();
             this.pa = new System.Windows.Forms.Label();
-            this.s = new System.Windows.Forms.Label();
             this.ndp = new System.Windows.Forms.Label();
             this.r = new System.Windows.Forms.Label();
-            this.ttva = new System.Windows.Forms.ComboBox();
-            this.tt = new System.Windows.Forms.Label();
-            this.nouveau = new System.Windows.Forms.Button();
             this.modifier = new System.Windows.Forms.Button();
             this.enregistrer = new System.Windows.Forms.Button();
             this.rec = new System.Windows.Forms.TextBox();
             this.rechercher = new System.Windows.Forms.Button();
             this.tableau = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ta = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -63,26 +49,15 @@ namespace FD_STOCK
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.color = new System.Windows.Forms.ComboBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.tableau)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button36
-            // 
-            this.button36.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button36.BackColor = System.Drawing.SystemColors.Control;
-            this.button36.FlatAppearance.BorderSize = 0;
-            this.button36.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
-            this.button36.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button36.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button36.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(50)))), ((int)(((byte)(103)))));
-            this.button36.Location = new System.Drawing.Point(967, 184);
-            this.button36.Name = "button36";
-            this.button36.Size = new System.Drawing.Size(25, 33);
-            this.button36.TabIndex = 184;
-            this.button36.Text = "-";
-            this.button36.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button36.UseVisualStyleBackColor = false;
-            this.button36.Visible = false;
             // 
             // label8
             // 
@@ -119,29 +94,18 @@ namespace FD_STOCK
             // 
             this.pah.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pah.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pah.Location = new System.Drawing.Point(163, 178);
+            this.pah.Location = new System.Drawing.Point(163, 173);
             this.pah.Name = "pah";
             this.pah.Size = new System.Drawing.Size(326, 22);
             this.pah.TabIndex = 171;
             this.pah.Text = "0.00";
             this.pah.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // st
-            // 
-            this.st.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.st.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.st.Location = new System.Drawing.Point(163, 150);
-            this.st.Name = "st";
-            this.st.Size = new System.Drawing.Size(326, 22);
-            this.st.TabIndex = 170;
-            this.st.Text = "0.00";
-            this.st.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            // 
             // nda
             // 
             this.nda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.nda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nda.Location = new System.Drawing.Point(163, 122);
+            this.nda.Location = new System.Drawing.Point(163, 145);
             this.nda.Name = "nda";
             this.nda.Size = new System.Drawing.Size(326, 22);
             this.nda.TabIndex = 169;
@@ -150,7 +114,7 @@ namespace FD_STOCK
             // 
             this.nr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.nr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nr.Location = new System.Drawing.Point(163, 94);
+            this.nr.Location = new System.Drawing.Point(163, 117);
             this.nr.Name = "nr";
             this.nr.Size = new System.Drawing.Size(326, 22);
             this.nr.TabIndex = 168;
@@ -159,27 +123,17 @@ namespace FD_STOCK
             // 
             this.pa.AutoSize = true;
             this.pa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pa.Location = new System.Drawing.Point(15, 184);
+            this.pa.Location = new System.Drawing.Point(15, 179);
             this.pa.Name = "pa";
             this.pa.Size = new System.Drawing.Size(114, 16);
             this.pa.TabIndex = 167;
             this.pa.Text = "Prix d\'achat HT";
             // 
-            // s
-            // 
-            this.s.AutoSize = true;
-            this.s.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.s.Location = new System.Drawing.Point(15, 156);
-            this.s.Name = "s";
-            this.s.Size = new System.Drawing.Size(47, 16);
-            this.s.TabIndex = 166;
-            this.s.Text = "Stock";
-            // 
             // ndp
             // 
             this.ndp.AutoSize = true;
             this.ndp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ndp.Location = new System.Drawing.Point(15, 128);
+            this.ndp.Location = new System.Drawing.Point(15, 151);
             this.ndp.Name = "ndp";
             this.ndp.Size = new System.Drawing.Size(142, 16);
             this.ndp.TabIndex = 165;
@@ -189,52 +143,11 @@ namespace FD_STOCK
             // 
             this.r.AutoSize = true;
             this.r.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.r.Location = new System.Drawing.Point(15, 100);
+            this.r.Location = new System.Drawing.Point(15, 123);
             this.r.Name = "r";
             this.r.Size = new System.Drawing.Size(116, 16);
             this.r.TabIndex = 164;
             this.r.Text = "N° de référence";
-            // 
-            // ttva
-            // 
-            this.ttva.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ttva.FormattingEnabled = true;
-            this.ttva.Items.AddRange(new object[] {
-            "20",
-            "14",
-            "10",
-            "  7",
-            "  0"});
-            this.ttva.Location = new System.Drawing.Point(163, 206);
-            this.ttva.Name = "ttva";
-            this.ttva.Size = new System.Drawing.Size(326, 24);
-            this.ttva.TabIndex = 181;
-            // 
-            // tt
-            // 
-            this.tt.AutoSize = true;
-            this.tt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tt.Location = new System.Drawing.Point(15, 214);
-            this.tt.Name = "tt";
-            this.tt.Size = new System.Drawing.Size(133, 16);
-            this.tt.TabIndex = 180;
-            this.tt.Text = "Taux de la tva (%)";
-            // 
-            // nouveau
-            // 
-            this.nouveau.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(50)))), ((int)(((byte)(103)))));
-            this.nouveau.FlatAppearance.BorderSize = 0;
-            this.nouveau.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.nouveau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nouveau.ForeColor = System.Drawing.Color.White;
-            this.nouveau.Location = new System.Drawing.Point(892, 233);
-            this.nouveau.Name = "nouveau";
-            this.nouveau.Size = new System.Drawing.Size(160, 34);
-            this.nouveau.TabIndex = 177;
-            this.nouveau.Text = "Nouveau";
-            this.nouveau.UseVisualStyleBackColor = false;
-            this.nouveau.Visible = false;
-            this.nouveau.Click += new System.EventHandler(this.nouveau_Click);
             // 
             // modifier
             // 
@@ -243,7 +156,7 @@ namespace FD_STOCK
             this.modifier.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.modifier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.modifier.ForeColor = System.Drawing.Color.White;
-            this.modifier.Location = new System.Drawing.Point(163, 236);
+            this.modifier.Location = new System.Drawing.Point(163, 210);
             this.modifier.Name = "modifier";
             this.modifier.Size = new System.Drawing.Size(160, 28);
             this.modifier.TabIndex = 176;
@@ -258,7 +171,7 @@ namespace FD_STOCK
             this.enregistrer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.enregistrer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.enregistrer.ForeColor = System.Drawing.Color.White;
-            this.enregistrer.Location = new System.Drawing.Point(329, 236);
+            this.enregistrer.Location = new System.Drawing.Point(329, 210);
             this.enregistrer.Name = "enregistrer";
             this.enregistrer.Size = new System.Drawing.Size(160, 28);
             this.enregistrer.TabIndex = 175;
@@ -301,59 +214,11 @@ namespace FD_STOCK
             this.Column10,
             this.Column2,
             this.Column3,
-            this.Column4,
-            this.Column8});
+            this.Column4});
             this.tableau.Location = new System.Drawing.Point(18, 353);
             this.tableau.Name = "tableau";
             this.tableau.Size = new System.Drawing.Size(1101, 357);
             this.tableau.TabIndex = 172;
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "N° de composant";
-            this.Column1.Name = "Column1";
-            this.Column1.Width = 130;
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Type de composant";
-            this.Column5.Name = "Column5";
-            this.Column5.Width = 130;
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "Couleur";
-            this.Column6.Name = "Column6";
-            // 
-            // Column10
-            // 
-            this.Column10.HeaderText = "N° de référence";
-            this.Column10.Name = "Column10";
-            this.Column10.Width = 130;
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Nom de composant";
-            this.Column2.Name = "Column2";
-            this.Column2.Width = 160;
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Stock";
-            this.Column3.Name = "Column3";
-            this.Column3.Width = 130;
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Prix d\'achat HT";
-            this.Column4.Name = "Column4";
-            this.Column4.Width = 130;
-            // 
-            // Column8
-            // 
-            this.Column8.HeaderText = "Taux de la tva (%)";
-            this.Column8.Name = "Column8";
-            this.Column8.Width = 130;
             // 
             // ta
             // 
@@ -363,7 +228,7 @@ namespace FD_STOCK
             "Matiére 1 ére",
             "Emballage",
             "Piéce de rechange"});
-            this.ta.Location = new System.Drawing.Point(163, 64);
+            this.ta.Location = new System.Drawing.Point(163, 87);
             this.ta.Name = "ta";
             this.ta.Size = new System.Drawing.Size(160, 24);
             this.ta.TabIndex = 186;
@@ -373,7 +238,7 @@ namespace FD_STOCK
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(15, 72);
+            this.label1.Location = new System.Drawing.Point(15, 95);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(124, 16);
             this.label1.TabIndex = 185;
@@ -433,11 +298,52 @@ namespace FD_STOCK
             "Vert ",
             "Violet",
             "Rose"});
-            this.color.Location = new System.Drawing.Point(329, 64);
+            this.color.Location = new System.Drawing.Point(329, 87);
             this.color.Name = "color";
             this.color.Size = new System.Drawing.Size(160, 24);
             this.color.TabIndex = 252;
             this.color.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "N° de composant";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 130;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Type de composant";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 130;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Couleur";
+            this.Column6.Name = "Column6";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "N° de référence";
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nom de composant";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 250;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Stock";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 130;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Prix d\'achat HT";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
             // 
             // npg
             // 
@@ -451,20 +357,14 @@ namespace FD_STOCK
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.ta);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button36);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pah);
-            this.Controls.Add(this.st);
             this.Controls.Add(this.nda);
             this.Controls.Add(this.nr);
             this.Controls.Add(this.pa);
-            this.Controls.Add(this.s);
             this.Controls.Add(this.ndp);
             this.Controls.Add(this.r);
-            this.Controls.Add(this.ttva);
-            this.Controls.Add(this.tt);
-            this.Controls.Add(this.nouveau);
             this.Controls.Add(this.modifier);
             this.Controls.Add(this.enregistrer);
             this.Controls.Add(this.rec);
@@ -482,21 +382,14 @@ namespace FD_STOCK
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button36;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox pah;
-        private System.Windows.Forms.TextBox st;
         private System.Windows.Forms.TextBox nda;
         private System.Windows.Forms.TextBox nr;
         private System.Windows.Forms.Label pa;
-        private System.Windows.Forms.Label s;
         private System.Windows.Forms.Label ndp;
         private System.Windows.Forms.Label r;
-        private System.Windows.Forms.ComboBox ttva;
-        private System.Windows.Forms.Label tt;
-        private System.Windows.Forms.Button nouveau;
         private System.Windows.Forms.Button modifier;
         private System.Windows.Forms.Button enregistrer;
         private System.Windows.Forms.TextBox rec;
@@ -516,6 +409,5 @@ namespace FD_STOCK
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
     }
 }
