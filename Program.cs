444143587f9618d7708170dc7cd937c8588bd194
aouirachid <aouirachid22@gmail.com>
2025-12-03 01:44:32 +0100

    update git ignore file

--- Program.cs
+++ Program.cs
@@ -43,8 +43,8 @@ namespace FD_STOCK
             // Application.Run(new lsem());
             // Application.Run(new MDIParent1());
             //Application.Run(new paymentFournisseur());
-            //Application.Run(new menur());
-            Application.Run(new lpg());
+            Application.Run(new menur());
+            //Application.Run(new lpg());
             //Application.Run(new utilisateur());
             //Application.Run(new listePieceRechange());
             // Application.Run(new nmach());
